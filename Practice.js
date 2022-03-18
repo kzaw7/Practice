ab5340b6758a4e14040500da9dd18ccce6bf8eba
kzaw7 <74020237+kzaw7@users.noreply.github.com>
2022-03-18 00:54:00 -0400

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("27");
+
+let first_Name_person = "Khine";
+console.log(first_Name_person);
+
+
+let myFirstJob = 'programmar';
+let myCurrentJob = 'teacher';
+
+
+
+
+
+
