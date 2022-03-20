abee3727bce9fcf42e971e86a49b4196d92a636a
kzaw7 <74020237+kzaw7@users.noreply.github.com>
2022-03-20 00:01:10 -0400

    Add files via upload

--- Practice.js
+++ Practice.js
@@ -8,8 +8,43 @@ let first_Name_person = "Khine";
 console.log(first_Name_person);
 
 
-let myFirstJob = 'programmar';
+let myFirstJob = 'programmer';
 let myCurrentJob = 'teacher';
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let country = 'Myanmar'
+let continent = 'Southeast Asia';
+let population = '54million';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Math Operator
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2028;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2);
+
+//assignment operator
+let x = 10 + 5;
+x += 10;
+x ++;
+x --;
+
+//Comparison operator 
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+/*
+coding challenege
+BMI = mass / height ** 2= mass / (height * height)
+
+mass in kg and height in meter
+*/
+Question 1: store Mark & John's mass & height in variables 
+2: calculate both their BMIs using the formula (you can even implement both versions)
+
 
 
 
