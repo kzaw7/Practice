2a877965dbffb12e7b7e0cdf44ed1cbbf6905fb1
kzaw7 <74020237+kzaw7@users.noreply.github.com>
2022-03-23 23:22:40 -0400

    Add files via upload

--- Practice.js
+++ Practice.js
@@ -41,9 +41,29 @@ coding challenege
 BMI = mass / height ** 2= mass / (height * height)
 
 mass in kg and height in meter
-*/
+
 Question 1: store Mark & John's mass & height in variables 
 2: calculate both their BMIs using the formula (you can even implement both versions)
+3: create a boolean variable 
+*/
+
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.68;
+const heightJohn = 1.95;
+
+const BMIMark = (massMark / heightMark ** 2);
+const BMIJohn = (massJohn / heightJohn ** 2);
+console.log(BMIMark, BMIJohn);
+
+const firstName = 'khine';
+const job = 'teacher';
+const birthYear = '1994';
+const year = '2037';
+
+const khine = "I'm " + firstName + ", a " + (year - birthYear)
+ + " year old " + job + "!";
+console.log(khine);
 
 
 
